2a9404ad7997a1aa4561b9763eb2f39513ef51e7
Curro Osuna T-t <120415631+Hard-Curro@users.noreply.github.com>
2024-01-07 22:33:10 +0100

    feature1

--- lib/widgets/contador.dart
+++ lib/widgets/contador.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class contador extends StatelessWidget {
-  const contador({super.key});
+class Contador extends StatefulWidget {
+  const Contador({Key? key}) : super(key: key);
+
+  @override
+  _ContadorState createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  int numero = 0;
+
+  void incrementar() {
+    setState(() {
+      numero += 1;
+    });
+  }
+
+  void decrementar() {
+    if (numero > 0) {
+      setState(() {
+        numero -= 1;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +31,9 @@ class contador extends StatelessWidget {
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Color.fromRGBO(104, 183, 247, 1)),
+        borderRadius: BorderRadius.circular(10),
+        color: Color.fromRGBO(104, 183, 247, 1),
+      ),
       child: Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.center,
@@ -22,15 +44,18 @@ class contador extends StatelessWidget {
             child: Center(
               child: Transform.translate(
                 offset: Offset(
-                    0, -30), // Ajusta el valor en Y según tus necesidades
+                  0,
+                  -30,
+                ),
                 child: Text(
-                  "13",
+                  "$numero",
                   textAlign: TextAlign.start,
                   style: GoogleFonts.poppins(
                     textStyle: TextStyle(
-                        fontSize: 330,
-                        fontWeight: FontWeight.w100,
-                        color: Colors.white),
+                      fontSize: 330,
+                      fontWeight: FontWeight.w100,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
@@ -39,68 +64,87 @@ class contador extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
+              GestureDetector(
+                onTap: incrementar,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(100),
                     color: Color.fromARGB(57, 255, 255, 255),
                     border: Border.all(
-                      color: Colors.white, // Color del borde blanco
-                      width: 3.0, // Ancho del borde
-                    )),
-                child: Center(
-                    child: Text(
-                  "+1",
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 70.0, // Tamaño del texto
-                    color: Colors.white, // Color del texto
+                      color: Colors.white,
+                      width: 3.0,
+                    ),
                   ),
-                )),
+                  child: Center(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 70.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
               ),
               SizedBox(
                 width: 130,
               ),
-              Container(
-                width: 80,
-                height: 80,
-                decoration: BoxDecoration(
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    numero = 0;
+                  });
+                },
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(100),
                     color: Color.fromARGB(57, 255, 255, 255),
                     border: Border.all(
-                      color: Colors.white, // Color del borde blanco
-                      width: 3.0, // Ancho del borde
-                    )),
-                child: Center(
+                      color: Colors.white,
+                      width: 3.0,
+                    ),
+                  ),
+                  child: Center(
                     child: Icon(
-                  Icons.refresh,
-                  color: Colors.white,
-                  size: 60,
-                )),
+                      Icons.refresh,
+                      color: Colors.white,
+                      size: 60,
+                    ),
+                  ),
+                ),
               ),
               SizedBox(
                 width: 130,
               ),
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
+              GestureDetector(
+                onTap: decrementar,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(100),
                     color: Color.fromARGB(57, 255, 255, 255),
                     border: Border.all(
-                      color: Colors.white, // Color del borde blanco
-                      width: 3.0, // Ancho del borde
-                    )),
-                child: Center(
-                    child: Text(
-                  "-1",
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 70.0, // Tamaño del texto
-                    color: Colors.white, // Color del texto
+                      color: Colors.white,
+                      width: 3.0,
+                    ),
                   ),
-                )),
+                  child: Center(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 70.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
               )
             ],
           )
